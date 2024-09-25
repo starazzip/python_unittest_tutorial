dd1aeca57e007224346f0e3c930ba9d989e7b553
azzip <azzip-rnd@skvip.org>
2024-09-25 16:39:37 +0800

    feat: 增加3A註解

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -2,25 +2,43 @@
 
 import os
 import sys
+import unittest
+
 TEST_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJECT_DIR = os.path.abspath(os.path.join(TEST_DIR, os.pardir))
 sys.path.insert(0, PROJECT_DIR)
-import unittest
+
 from src.calculator import Calculator
+
 
 class TestCalculator(unittest.TestCase):
 
     def test_add(self):
+        # Arrange
         calc = Calculator()
-        self.assertEqual(calc.add(2, 3), 5)
-        self.assertEqual(calc.add(-1, 1), 0)
+
+        # Action
+        result1 = calc.add(2, 3)
+        result2 = calc.add(-1, 1)
+
+        # Assert
+        self.assertEqual(result1, 5)
+        self.assertEqual(result2, 0)
 
     def test_divide(self):
+        # Arrange
         calc = Calculator()
-        self.assertEqual(calc.divide(10, 2), 5)
 
+        # Action
+        result = calc.divide(10, 2)
+
+        # Assert
+        self.assertEqual(result, 5)
+
+        # Action & Assert
         with self.assertRaises(ZeroDivisionError):
             calc.divide(10, 0)
+
 
 if __name__ == '__main__':
     unittest.main()
--- tests/test_use_mock.py
+++ tests/test_use_mock.py
@@ -1,23 +1,30 @@
 # tests/test_calculator.py
 import os
 import sys
+
 TEST_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJECT_DIR = os.path.abspath(os.path.join(TEST_DIR, os.pardir))
 sys.path.insert(0, PROJECT_DIR)
 import unittest
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 
 from src.calculator import Calculator
+
 
 class TestMockCalculator(unittest.TestCase):
     @patch('src.calculator.requests.get')
     def test_get_exchange_rate(self, mock_get):
+        # # Arrange
         mock_response = mock_get.return_value
         mock_response.json.return_value = {'rates': {'USD': 1.1}}
-
         calc = Calculator()
+
+        # Action
         rate = calc.get_exchange_rate('EUR')
+
+        # Assert
         self.assertEqual(rate, 1.1)
+
 
 if __name__ == '__main__':
     unittest.main()
